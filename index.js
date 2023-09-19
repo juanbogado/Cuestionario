0f7e34e3916c1a2def8ff598e840fca2333907c6
Juanbogado <bogado.jmanuel@gmail.com>
2023-09-19 14:58:20 -0300

    cuestionario

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-// Función para mostrar una pregunta y opciones
-function mostrarPregunta(pregunta, opciones) {
+// Función para mostrar una pregunta
+function mostrarPregunta(pregunta) {
     alert(pregunta);
-    for (let i = 0; i < opciones.length; i++) {
-        alert(`${i + 1}. ${opciones[i]}`);
-    }
 }
 
-// Función para validar la respuesta
+// Función para la respuesta
 function validarRespuesta(respuesta, respuestaCorrecta) {
     return respuesta === respuestaCorrecta;
 }
 
-// Función principal
-function main() {
-    alert("Bienvenido al Cuestionario");
-
+// Función para realizar el cuestionario
+function realizarCuestionario() {
     const preguntas = [
         {
-            pregunta: "¿Cuál es la posición  de facundo conte en voley?",
-            opciones: ["Armador", "libero", "Punta receptor"],
+            pregunta: "¿Cuál es la posición de Facundo Conte en voleibol? \n1- Armador \n2- Libero \n3- Punta receptor",
             respuestaCorrecta: 3,
         },
         {
-            pregunta: "¿Cuál es el mejor club de futbol de Argentina?",
-            opciones: ["River", "Boca Junior", "Velez"],
+            pregunta: "¿Cuál es el mejor club de futbol de Argentina? \n1- River \n2- Boca Junior \n3- Velez",
             respuestaCorrecta: 2,
         },
     ];
@@ -32,20 +25,37 @@ function main() {
 
     for (let i = 0; i < preguntas.length; i++) {
         const preguntaActual = preguntas[i];
-        mostrarPregunta(preguntaActual.pregunta, preguntaActual.opciones);
+        mostrarPregunta(preguntaActual.pregunta);
         const respuesta = parseInt(prompt("Ingrese el número de su respuesta:"));
 
         if (validarRespuesta(respuesta, preguntaActual.respuestaCorrecta)) {
             alert("¡Respuesta correcta!");
             puntaje++;
         } else {
-            alert("Respuesta incorrecta. La respuesta correcta era la opción " + preguntaActual.respuestaCorrecta);
+            alert(`Respuesta incorrecta. La respuesta correcta era la opción ${preguntaActual.respuestaCorrecta}`);
         }
     }
 
     alert(`Fin del cuestionario. Puntaje: ${puntaje} de ${preguntas.length}`);
     console.log(`Puntaje: ${puntaje} de ${preguntas.length}`);
+
+    // Ejemplo de uso de objetos, arrays y funciones de orden superior
+    const usuarios = [
+        { nombre: "Juan", puntaje: 2 },
+        { nombre: "María", puntaje: 1 },
+        { nombre: "Carlos", puntaje: 2 },
+    ];
+
+    const usuariosAprobados = usuarios.filter(usuario => usuario.puntaje >= 2);
+    console.log("Usuarios aprobados:", usuariosAprobados);
+
+    const nombresAprobados = usuariosAprobados.map(usuario => usuario.nombre);
+    console.log("Nombres de usuarios aprobados:", nombresAprobados);
+
+    usuarios.forEach(usuario => {
+        console.log(`${usuario.nombre} tiene un puntaje de ${usuario.puntaje}`);
+    });
 }
 
-// Llamamos a la función principal
-main();
+
+realizarCuestionario();
