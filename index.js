f4f101649a3a9efc0dd72a41a7f9f2c2165b84dd
Juanbogado <bogado.jmanuel@gmail.com>
2023-10-05 13:48:15 -0300

    modificaciones de la entrega anterior

--- index.js
+++ index.js
@@ -1,71 +1,29 @@
-// // Función para mostrar una pregunta
-// function mostrarPregunta(pregunta) {
-//     alert(pregunta);
-// }
+// Variables
+let puntaje = 0;
+let preguntaIndex = 0;
+const tablaPosiciones = [];
 
-// // Función para la respuesta
-// function validarRespuesta(respuesta, respuestaCorrecta) {
-//     return respuesta === respuestaCorrecta;
-// }
-
-// // Función para realizar el cuestionario
-// function realizarCuestionario() {
-//     const preguntas = [
-//         {
-//             pregunta: "¿Cuál es la posición de Facundo Conte en voleibol? \n1- Armador \n2- Libero \n3- Punta receptor",
-//             respuestaCorrecta: 3,
-//         },
-//         {
-//             pregunta: "¿Cuál es el mejor club de futbol de Argentina? \n1- River \n2- Boca Junior \n3- Velez",
-//             respuestaCorrecta: 2,
-//         },
-//     ];
-
-//     let puntaje = 0;
-
-//     for (let i = 0; i < preguntas.length; i++) {
-//         const preguntaActual = preguntas[i];
-//         mostrarPregunta(preguntaActual.pregunta);
-//         const respuesta = parseInt(prompt("Ingrese el número de su respuesta:"));
-
-//         if (validarRespuesta(respuesta, preguntaActual.respuestaCorrecta)) {
-//             alert("¡Respuesta correcta!");
-//             puntaje++;
-//         } else {
-//             alert(`Respuesta incorrecta. La respuesta correcta era la opción ${preguntaActual.respuestaCorrecta}`);
-//         }
-//     }
-
-//     alert(`Fin del cuestionario. Puntaje: ${puntaje} de ${preguntas.length}`);
-//     console.log(`Puntaje: ${puntaje} de ${preguntas.length}`);
-
-//     // Ejemplo de uso de objetos, arrays y funciones de orden superior
-//     const usuarios = [
-//         { nombre: "Juan", puntaje: 2 },
-//         { nombre: "María", puntaje: 1 },
-//         { nombre: "Carlos", puntaje: 2 },
-//     ];
-
-//     const usuariosAprobados = usuarios.filter(usuario => usuario.puntaje >= 2);
-//     console.log("Usuarios aprobados:", usuariosAprobados);
-
-//     const nombresAprobados = usuariosAprobados.map(usuario => usuario.nombre);
-//     console.log("Nombres de usuarios aprobados:", nombresAprobados);
-
-//     usuarios.forEach(usuario => {
-//         console.log(`${usuario.nombre} tiene un puntaje de ${usuario.puntaje}`);
-//     });
-// }
-
-
-// realizarCuestionario();
+// Preguntas y respuestas
+const preguntas = [
+    {
+        pregunta: "¿Cuál es la posición de Facundo Conte en voleibol?",
+        opciones: ["Armador", "Libero", "Punta receptor"],
+        respuestaCorrecta: 3,
+    },
+    {
+        pregunta: "¿Cuál es el rol que cumple el armador en un partido de voley?",
+        opciones: ["Recibir Atacar", "Armar juego para los atacantes", "Solo recibir"],
+        respuestaCorrecta: 2,
+    },
+];
 
 // Función para mostrar una pregunta y opciones
-function mostrarPregunta(pregunta, opciones) {
-    document.getElementById('pregunta').textContent = pregunta;
+function mostrarPregunta() {
+    const preguntaActual = preguntas[preguntaIndex];
+    document.getElementById('pregunta').textContent = preguntaActual.pregunta;
     const opcionesList = document.getElementById('opciones');
     opcionesList.innerHTML = '';
-    opciones.forEach((opcion, index) => {
+    preguntaActual.opciones.forEach((opcion, index) => {
         const li = document.createElement('li');
         li.textContent = `${index + 1}. ${opcion}`;
         opcionesList.appendChild(li);
@@ -73,64 +31,63 @@ function mostrarPregunta(pregunta, opciones) {
 }
 
 // Función para validar la respuesta
-function validarRespuesta(respuesta, respuestaCorrecta) {
-    return respuesta === respuestaCorrecta;
+function validarRespuesta(respuesta) {
+    const preguntaActual = preguntas[preguntaIndex];
+    return respuesta === preguntaActual.respuestaCorrecta;
 }
 
-// Función para realizar el cuestionario
-function realizarCuestionario() {
-    const preguntas = [
-        {
-            pregunta: "¿Cuál es la posición de Facundo Conte en voleibol?",
-            opciones: ["Armador", "Libero", "Punta receptor"],
-            respuestaCorrecta: 3,
-        },
-        {
-            pregunta: "¿Cuál es el color del cielo en un día despejado?",
-            opciones: ["Verde", "Azul", "Rojo"],
-            respuestaCorrecta: 2,
-        },
-    ];
+// Función para mostrar el resultado y actualizar la tabla de posiciones
+function mostrarResultado() {
+    const nombre = document.getElementById('nombre').value;
+    const resultadoText = `¡${nombre}, has obtenido ${puntaje} puntos!`;
+    document.getElementById('resultado').textContent = resultadoText;
+    document.getElementById('resultado-container').style.display = 'block';
 
-    let puntaje = 0;
-    let preguntaIndex = 0;
+    // Agregar el puntaje a la tabla de posiciones
+    tablaPosiciones.push({ nombre, puntaje });
 
-    const submitButton = document.getElementById('submit');
-    const respuestaInput = document.getElementById('respuesta');
-    const resultadoContainer = document.getElementById('resultado');
+    // Ordenar la tabla de posiciones por puntaje descendente
+    tablaPosiciones.sort((a, b) => b.puntaje - a.puntaje);
 
-    submitButton.addEventListener('click', () => {
-        const respuesta = parseInt(respuestaInput.value);
-        if (validarRespuesta(respuesta, preguntas[preguntaIndex].respuestaCorrecta)) {
-            resultadoContainer.textContent = "¡Respuesta correcta!";
-            puntaje++;
-        } else {
-            resultadoContainer.textContent = `Respuesta incorrecta. La respuesta correcta era la opción ${preguntas[preguntaIndex].respuestaCorrecta}`;
-        }
-        preguntaIndex++;
-        if (preguntaIndex < preguntas.length) {
-            mostrarPregunta(preguntas[preguntaIndex].pregunta, preguntas[preguntaIndex].opciones);
-        } else {
-            resultadoContainer.textContent = `Fin del cuestionario. Puntaje: ${puntaje} de ${preguntas.length}`;
-            console.log(`Puntaje: ${puntaje} de ${preguntas.length}`);
-
-            // Almacenar el puntaje en el almacenamiento local
-            localStorage.setItem('puntaje', puntaje);
-
-            // Convertir y almacenar los datos de los usuarios en formato JSON
-            const usuarios = [
-                { nombre: "Juan", puntaje: 2 },
-                { nombre: "María", puntaje: 1 },
-                { nombre: "Carlos", puntaje: 2 },
-            ];
-            const usuariosJSON = JSON.stringify(usuarios);
-            localStorage.setItem('usuarios', usuariosJSON);
-        }
-    });
-
-    // Mostrar la primera pregunta al cargar la página
-    mostrarPregunta(preguntas[preguntaIndex].pregunta, preguntas[preguntaIndex].opciones);
+    // Mostrar el top 3 de la tabla de posiciones
+    const tablaPosicionesList = document.getElementById('tabla-posiciones');
+    tablaPosicionesList.innerHTML = '';
+    for (let i = 0; i < Math.min(3, tablaPosiciones.length); i++) {
+        const li = document.createElement('li');
+        li.textContent = `${tablaPosiciones[i].nombre}: ${tablaPosiciones[i].puntaje} puntos`;
+        tablaPosicionesList.appendChild(li);
+    }
 }
 
-// Llamamos a la función principal para realizar el cuestionario
-realizarCuestionario();
+// Función para manejar el evento de inicio
+document.getElementById('iniciar').addEventListener('click', () => {
+    const nombre = document.getElementById('nombre').value;
+    if (nombre.trim() === '') {
+        alert('Por favor, ingresa tu nombre.');
+    } else {
+        // Ocultar el formulario de inicio y mostrar el cuestionario
+        document.getElementById('inicio-container').style.display = 'none';
+        document.getElementById('cuestionario-container').style.display = 'block';
+
+        // Mostrar la primera pregunta
+        mostrarPregunta();
+    }
+});
+
+// Función para manejar el evento de envío de respuesta
+document.getElementById('submit').addEventListener('click', () => {
+    const respuesta = parseInt(document.getElementById('respuesta').value);
+    if (validarRespuesta(respuesta)) {
+        puntaje++;
+        alert("¡Respuesta correcta!");
+    } else {
+        alert(`Respuesta incorrecta. La respuesta correcta era la opción ${preguntas[preguntaIndex].respuestaCorrecta}`);
+    }
+    preguntaIndex++;
+
+    if (preguntaIndex < preguntas.length) {
+        mostrarPregunta();
+    } else {
+        mostrarResultado();
+    }
+});
