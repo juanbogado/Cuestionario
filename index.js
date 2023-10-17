7f57182797029c12e49cea0d222ff2644652297b
Juanbogado <bogado.jmanuel@gmail.com>
2023-10-17 15:50:27 -0300

    correcion de la preentrega

--- index.js
+++ index.js
@@ -1,93 +1,99 @@
-// Variables
-let puntaje = 0;
-let preguntaIndex = 0;
-const tablaPosiciones = [];
-
-// Preguntas y respuestas
+// Datos del cuestionario
 const preguntas = [
     {
-        pregunta: "¿Cuál es la posición de Facundo Conte en voleibol?",
-        opciones: ["Armador", "Libero", "Punta receptor"],
-        respuestaCorrecta: 3,
+        pregunta: "¿Cuantó toques se puede hacer en voley?",
+        opciones: ["2", "5", "3"],
+        respuestaCorrecta: "3"
     },
     {
-        pregunta: "¿Cuál es el rol que cumple el armador en un partido de voley?",
-        opciones: ["Recibir Atacar", "Armar juego para los atacantes", "Solo recibir"],
-        respuestaCorrecta: 2,
+        pregunta: "¿En un equipo de voley, que rol cumple el central?",
+        opciones: ["Llevar agua a sus compañeros", "Bloquear y atacar", "Recibir los ataques"],
+        respuestaCorrecta: "Bloquear y atacar"
     },
+    {
+        pregunta: "¿En que año la seleccion de voley masculina gano la medalla de broce en los juegos olimpicos?",
+        opciones: ["2020", "2018", "2022"],
+        respuestaCorrecta: "2020"
+    }
 ];
 
-// Función para mostrar una pregunta y opciones
-function mostrarPregunta() {
-    const preguntaActual = preguntas[preguntaIndex];
-    document.getElementById('pregunta').textContent = preguntaActual.pregunta;
-    const opcionesList = document.getElementById('opciones');
-    opcionesList.innerHTML = '';
-    preguntaActual.opciones.forEach((opcion, index) => {
-        const li = document.createElement('li');
-        li.textContent = `${index + 1}. ${opcion}`;
-        opcionesList.appendChild(li);
-    });
-}
+// Variables
+let nombre = "";
+let puntaje = 0;
+let preguntaActual = 0;
+const nombreInput = document.getElementById("nombreInput");
+const iniciarBtn = document.getElementById("iniciarBtn");
+const cuestionario = document.getElementById("cuestionario");
+const preguntaElement = document.getElementById("pregunta");
+const opcionesElement = document.getElementById("opciones");
+const siguienteBtn = document.getElementById("siguienteBtn");
+const resultado = document.getElementById("resultado");
+const puntajeElement = document.getElementById("puntaje");
+const reiniciarBtn = document.getElementById("reiniciarBtn");
 
-// Función para validar la respuesta
-function validarRespuesta(respuesta) {
-    const preguntaActual = preguntas[preguntaIndex];
-    return respuesta === preguntaActual.respuestaCorrecta;
-}
-
-// Función para mostrar el resultado y actualizar la tabla de posiciones
-function mostrarResultado() {
-    const nombre = document.getElementById('nombre').value;
-    const resultadoText = `¡${nombre}, has obtenido ${puntaje} puntos!`;
-    document.getElementById('resultado').textContent = resultadoText;
-    document.getElementById('resultado-container').style.display = 'block';
-
-    // Agregar el puntaje a la tabla de posiciones
-    tablaPosiciones.push({ nombre, puntaje });
-
-    // Ordenar la tabla de posiciones por puntaje descendente
-    tablaPosiciones.sort((a, b) => b.puntaje - a.puntaje);
-
-    // Mostrar el top 3 de la tabla de posiciones
-    const tablaPosicionesList = document.getElementById('tabla-posiciones');
-    tablaPosicionesList.innerHTML = '';
-    for (let i = 0; i < Math.min(3, tablaPosiciones.length); i++) {
-        const li = document.createElement('li');
-        li.textContent = `${tablaPosiciones[i].nombre}: ${tablaPosiciones[i].puntaje} puntos`;
-        tablaPosicionesList.appendChild(li);
-    }
-}
-
-// Función para manejar el evento de inicio
-document.getElementById('iniciar').addEventListener('click', () => {
-    const nombre = document.getElementById('nombre').value;
-    if (nombre.trim() === '') {
-        alert('Por favor, ingresa tu nombre.');
-    } else {
-        // Ocultar el formulario de inicio y mostrar el cuestionario
-        document.getElementById('inicio-container').style.display = 'none';
-        document.getElementById('cuestionario-container').style.display = 'block';
-
-        // Mostrar la primera pregunta
+// Al hacer clic en el botón de inicio
+iniciarBtn.addEventListener("click", () => {
+    nombre = nombreInput.value;
+    if (nombre) {
+        cuestionario.style.display = "block";
+        iniciarBtn.style.display = "none";
         mostrarPregunta();
     }
 });
 
-// Función para manejar el evento de envío de respuesta
-document.getElementById('submit').addEventListener('click', () => {
-    const respuesta = parseInt(document.getElementById('respuesta').value);
-    if (validarRespuesta(respuesta)) {
-        puntaje++;
-        alert("¡Respuesta correcta!");
-    } else {
-        alert(`Respuesta incorrecta. La respuesta correcta era la opción ${preguntas[preguntaIndex].respuestaCorrecta}`);
-    }
-    preguntaIndex++;
+// Mostrar la pregunta actual
+function mostrarPregunta() {
+    const pregunta = preguntas[preguntaActual];
+    preguntaElement.textContent = pregunta.pregunta;
+    opcionesElement.innerHTML = "";
+    pregunta.opciones.forEach((opcion, index) => {
+        const opcionBtn = document.createElement("button");
+        opcionBtn.textContent = opcion;
+        opcionBtn.addEventListener("click", () => {
+            verificarRespuesta(opcion);
+        });
+        opcionesElement.appendChild(opcionBtn);
+    });
+}
 
-    if (preguntaIndex < preguntas.length) {
+// Verificar si la respuesta es correcta
+function verificarRespuesta(respuesta) {
+    const pregunta = preguntas[preguntaActual];
+    if (respuesta === pregunta.respuestaCorrecta) {
+        puntaje++;
+    }
+    preguntaActual++;
+    if (preguntaActual < preguntas.length) {
         mostrarPregunta();
     } else {
         mostrarResultado();
     }
+}
+
+// Mostrar el resultado del cuestionario
+function mostrarResultado() {
+    cuestionario.style.display = "none";
+    resultado.style.display = "block";
+    puntajeElement.textContent = `¡${nombre}, obtuviste ${puntaje} de ${preguntas.length} puntos!`;
+
+    // Almacenar el puntaje en el almacenamiento local
+    const puntajesAnteriores = JSON.parse(localStorage.getItem("puntajes")) || [];
+    puntajesAnteriores.push({ nombre, puntaje });
+    localStorage.setItem("puntajes", JSON.stringify(puntajesAnteriores));
+}
+
+// Reiniciar el cuestionario
+reiniciarBtn.addEventListener("click", () => {
+    reiniciarCuestionario();
 });
+
+// Reiniciar el cuestionario y puntaje
+function reiniciarCuestionario() {
+    puntaje = 0;
+    preguntaActual = 0;
+    resultado.style.display = "none";
+    cuestionario.style.display = "block";
+    nombreInput.value = "";
+    nombre = "";
+    mostrarPregunta();
+}
